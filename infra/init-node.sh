0ff8b464c81e888c3d6da873a59efd5c6948236f
nchern <nchern@inbox.ru>
2020-07-13 13:11:27 +0200

    adds nginx as balancer

--- infra/init-node.sh
+++ infra/init-node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get -y update && apt-get -y install fail2ban docker.io git
+apt-get -y update && apt-get -y install fail2ban docker.io git make screen
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
