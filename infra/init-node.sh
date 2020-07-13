e12ad012454a0b32e478dca9335ded5b0439e2dc
nchern <nchern@inbox.ru>
2020-07-13 18:17:30 +0200

    updates init script

--- infra/init-node.sh
+++ infra/init-node.sh
@@ -4,3 +4,5 @@ apt-get -y update && apt-get -y install fail2ban docker.io git make screen
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir -p /btm/ssl  # store ssl certs here
